1449ff7b24feb9f9abe6cb02c854edddc1b7e1b4
Azim3triko <azim3triko@protonmail.com>
2022-08-19 11:55:32 +0000

    se arreglo estilo

--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -2,109 +2,97 @@ import React from "react";
 
 export function Cards() {
 	return (
-		<div className="card-group p-3">
-			<div
-				className="Cards text-center"
-				style={{
-					width: "18rem",
-				}}>
-				<img
-					className="card"
-					style={{
-						width: "13rem",
-						height: "10rem",
-						backgroundColor: "#d9d9d9",
-						margin: "2rem",
-						display: "-ms-flexbox",
-					}}
-				/>
-
-				<h5 className="card text-center">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Find Out More!
-				</a>
-			</div>
-
-			<div
-				className="Cards text-center"
-				style={{
-					width: "18rem",
-				}}>
-				<img
-					className="card"
-					style={{
-						width: "13rem",
-						height: "10rem",
-						backgroundColor: "#d9d9d9",
-						margin: "2rem",
-						display: "-ms-flexbox",
-					}}
-				/>
-
-				<h5 className="card text-center">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Find Out More!
-				</a>
-			</div>
-
-			<div
-				className="Cards text-center"
-				style={{
-					width: "18rem",
-				}}>
-				<img
-					className="card"
-					style={{
-						width: "13rem",
-						height: "10rem",
-						backgroundColor: "#d9d9d9",
-						margin: "2rem",
-						display: "-ms-flexbox",
-					}}
-				/>
-
-				<h5 className="card text-center">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Find Out More!
-				</a>
-			</div>
-
-			<div
-				className="Cards text-center"
-				style={{
-					width: "18rem",
-				}}>
-				<img
-					className="card"
-					style={{
-						width: "13rem",
-						height: "10rem",
-						backgroundColor: "#d9d9d9",
-						margin: "2rem",
-						display: "-ms-flexbox",
-					}}
-				/>
-
-				<h5 className="card text-center">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Find Out More!
-				</a>
+		<div className="container">
+			{/* <div className="justifi-content-center m-0 row my-3">
+				<div
+					className="card m-2"
+					style={{ backgroundColor: "#d9d9d9" }}>
+					<div className="card-body">
+						<h1 className="card-title fs-1">{"A Warm Welcome!"}</h1>
+						<p className="card-text fs-5">
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Ipsa, ipsam,eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat."
+							}
+						</p>
+						<a href="#" className="btn btn-primary fs-5">
+							{"Call to action!"}
+						</a>
+					</div>
+				</div>
+			</div> */}
+			<div className="d-sm-flex">
+				<div className="card text-center">
+					<img
+						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h4 className="card-title">{"Card title"}</h4>
+						<p className="card-text">
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
+						</p>
+						<a href="#" className="btn btn-primary">
+							{"Find Out More!"}
+						</a>
+					</div>
+				</div>
+				<div className="card mx-1 text-center">
+					<img
+						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h4 className="card-title">{"Card title"}</h4>
+						<p className="card-text">
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
+						</p>
+						<a href="#" className="btn btn-primary">
+							{"Find Out More!"}
+						</a>
+					</div>
+				</div>
+				<div className="card me-1 text-center">
+					<img
+						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h4 className="card-title">{"Card title"}</h4>
+						<p className="card-text">
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
+						</p>
+						<a href="#" className="btn btn-primary">
+							{"Find Out More!"}
+						</a>
+					</div>
+				</div>
+				<div className="card text-center">
+					<img
+						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body ">
+						<h4 className="card-title">{"Card title"}</h4>
+						<p className="card-text">
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
+						</p>
+						<a href="#" className="btn btn-primary mx-auto">
+							{"Find Out More!"}
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -2,14 +2,25 @@ import React from "react";
 
 export function Jumbotron() {
 	return (
-		<div className="container p-4" style={{ backgroundColor: "#d9d9d9" }}>
-			<h1 style={{ fontSize: "48px" }}>A Warm Welcome!</h1>
-			<h5>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry.Various versions have evolved over the years, sometimes
-				by accident, sometimes on purpose.
-			</h5>
-			<buttom className="btn btn-primary">Call to action!</buttom>
+		<div className="container">
+			<div className="justifi-content-center m-0 row my-3">
+				<div
+					className="card m-2"
+					style={{ backgroundColor: "#d9d9d9" }}>
+					<div className="card-body">
+						<h1 className="card-title fs-1">{"A Warm Welcome!"}</h1>
+						<p className="card-text fs-5">
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Ipsa, ipsam,eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat."
+							}
+						</p>
+						<a href="#" className="btn btn-primary fs-5">
+							{"Call to action!"}
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
+		// );
 	);
 }
